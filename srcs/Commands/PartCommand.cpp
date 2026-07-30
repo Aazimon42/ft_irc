06f9da3fd6a5378ff5c4c91cee82885c237cd604
Enzo Di Maiolo <edi-maio@3F1.42angouleme.fr>
2026-07-30 02:35:23 +0200

    part done

--- srcs/Commands/PartCommand.cpp
+++ srcs/Commands/PartCommand.cpp
@@ -6,11 +6,12 @@
 /*   By: edi-maio <edi-maio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/24 00:00:00 by edi-maio          #+#    #+#             */
-/*   Updated: 2026/07/24 23:31:51 by edi-maio         ###   ########.fr       */
+/*   Updated: 2026/07/30 02:35:11 by edi-maio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Commands/PartCommand.hpp"
+#include "Server.hpp"
 
 PartCommand::PartCommand(Server *server, Client *client, std::vector<std::string> args) : Command(server, client, args)
 {}
@@ -20,7 +21,31 @@ PartCommand::~PartCommand()
 
 void PartCommand::execute()
 {
-    (void)server;
-    (void)client;
-    (void)args;
+    if (args.size() < 2)
+    {
+        server->sendError(client->getFd(), 431, client->getNickname(), "");
+        return;
+    }
+    std::vector<std::string> channels = split(args[1], ',');
+    for (size_t i = 0; i < channels.size(); ++i)
+    {
+        std::string channelName = channels[i];
+        if (channelName.empty())
+        {
+            server->sendError(client->getFd(), 476, client->getNickname(), channelName);
+            continue;
+        }
+        Channel *channel = server->getChannel(channelName);
+        if (!channel)
+        {
+            server->sendError(client->getFd(), 403, client->getNickname(), channelName);
+            continue;
+        }
+        if (!channel->isInChannel(client))
+        {
+            server->sendError(client->getFd(), 442, client->getNickname(), channelName);
+            continue;
+        }
+        channel->removeClient(client);
+    }
 }
